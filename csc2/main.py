f67cbe428af21bd373122186c64ac0d2e0e7bc27
jaydenknibbs <129121370+jaydenknibbs@users.noreply.github.com>
2024-03-04 14:25:01 +1300

    Add files via upload

---
+++ csc2/main.py
@@ -0,0 +1,12 @@
+#functions will be here 
+
+
+#main stuff goes under here
+Instructions = input ('Did you want to read the instructions? ')
+print ('Instructions')
+if Instructions == 'yes':
+    print ('show_instructions')
+elif  Instructions == 'no':
+    pass
+else:
+    print ('Please answer Yes or No')
