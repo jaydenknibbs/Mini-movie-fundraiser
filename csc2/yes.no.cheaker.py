b02f2476875dabcc8f0706e5d1f70d5408636f47
jaydenknibbs <129121370+jaydenknibbs@users.noreply.github.com>
2024-03-07 12:09:11 +1300

    Add files via upload

---
+++ csc2/yes.no.cheaker.py
@@ -0,0 +1,12 @@
+#functions will be here 
+
+
+#main stuff goes under here
+Instructions = input ('Did you want to read the instructions? ').lower()
+print ('Instructions')
+if Instructions == 'yes':
+    print ('show_instructions')
+elif  Instructions == 'no':
+    pas s
+else:
+    print ('Please answer Yes or No')
