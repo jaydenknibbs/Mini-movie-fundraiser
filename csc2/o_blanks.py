57c8d5d03501aeabf0fa83bce407954d26ca9d1b
jaydenknibbs <129121370+jaydenknibbs@users.noreply.github.com>
2024-03-20 10:34:23 +1300

    Add files via upload

--- csc2/o_blanks.py
+++ csc2/o_blanks.py
@@ -6,5 +6,16 @@ def not_blank (qestion):
             print ('sorry this cant be blank. please try again.')
         else:
             return response
-        
-#main routine goes here
+  
+        while True:
+            response = input(qestion)
+            if response == "":
+                print ('sorry this cant be blank')
+            else:
+                return response
+
+while True:
+    name = not_blank (" enter name or type xxx to quite ")
+    if name == 'xxx':
+        break
+print ('we are done')
